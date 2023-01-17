a1fe20160b1dcf985664fa5f0a1320fe2091787a
DanteZZ <me@dantezz.ru>
2023-01-17 13:21:44 +0700

    init

---
+++ src/chat.ts
@@ -0,0 +1,166 @@
+import { WebSocket } from "uWebSockets.js";
+import { WsapixChannel } from "wsapix";
+import { Type, Static } from "@sinclair/typebox";
+
+export interface IClientState {
+  userId: string;
+  name: string;
+}
+
+const chatSchema = {
+  params: {
+    roomId: {
+      schema: { type: "string" },
+      description: "Room id",
+    },
+  },
+  query: {
+    name: {
+      type: "string",
+      description: "User display name",
+    },
+  },
+};
+
+export const chat = new WsapixChannel<WebSocket, IClientState>({
+  path: "/chat/{roomId}",
+  schema: chatSchema,
+});
+
+chat.use((client) => {
+  // check auth
+  if (!client.queryParams?.name) {
+    client.send({ type: "error", message: "Wrong token!", code: 401 });
+    return client.terminate(4003);
+  }
+  client.state = {
+    userId: Date.now().toString(36),
+    name: client.queryParams.name,
+  };
+});
+
+/* Client messages */
+
+// message from client
+export const userMessageSchema = {
+  $id: "user:message",
+  description: "New user message",
+  payload: Type.Strict(
+    Type.Object(
+      {
+        type: Type.String({
+          const: "user:message",
+          description: "Message type",
+        }),
+        text: Type.String({ description: "Message text" }),
+      },
+      { $id: "user:message" }
+    )
+  ),
+};
+
+export type UserMessageSchema = Static<typeof userMessageSchema.payload>;
+
+chat.clientMessage<UserMessageSchema>(
+  { type: "user:message" },
+  userMessageSchema,
+  (client, data) => {
+    chat.clients.forEach((c) => {
+      if (c === client) {
+        return;
+      }
+      c.send({
+        type: "chat:message",
+        userId: client.state.userId,
+        text: data.text,
+      });
+    });
+  }
+);
+
+/* Server messages */
+
+// New chat message
+
+export const chatMessageSchema = {
+  $id: "chat:message",
+  description: "New message in chat",
+  payload: Type.Strict(
+    Type.Object(
+      {
+        type: Type.String({
+          const: "chat:message",
+          description: "Message type",
+        }),
+        userId: Type.String({ description: "User Id" }),
+        text: Type.String({ description: "Message text" }),
+      },
+      { $id: "chat:message" }
+    )
+  ),
+};
+
+chat.serverMessage({ type: "chat:message" }, chatMessageSchema);
+
+// User connect message
+
+export const userConnectedSchema = {
+  $id: "user:connected",
+  description: "User online status update",
+  payload: Type.Strict(
+    Type.Object(
+      {
+        type: Type.String({
+          const: "user:connected",
+          description: "Message type",
+        }),
+        userId: Type.String({ description: "User id" }),
+        name: Type.String({ description: "User name" }),
+      },
+      { $id: "user:connected" }
+    )
+  ),
+};
+
+chat.serverMessage({ type: "user:connected" }, userConnectedSchema);
+
+chat.on("connect", (client) => {
+  chat.clients.forEach((c) => {
+    if (c === client) {
+      return;
+    }
+    c.send({ type: "user:connected", ...client.state });
+    client.send({ type: "user:connected", ...c.state });
+  });
+});
+
+// User disconnect message
+
+export const userDisconnectedSchema = {
+  $id: "user:disconnected",
+  description: "User online status update",
+  payload: Type.Strict(
+    Type.Object(
+      {
+        type: Type.String({
+          const: "user:disconnected",
+          description: "Message type",
+        }),
+        userId: Type.String({ description: "User Id" }),
+        name: Type.String({ description: "User name" }),
+      },
+      { $id: "user:disconnected" }
+    )
+  ),
+};
+
+chat.serverMessage({ type: "user:disconnected" }, userDisconnectedSchema);
+
+chat.on("disconnect", (client) => {
+  chat.clients.forEach((c) => {
+    if (c === client) {
+      return;
+    }
+    c.send({ type: "user:disconnected", ...client.state });
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import { Type } from "@sinclair/typebox";
+import { App } from "uWebSockets.js";
+import { Wsapix } from "wsapix";
+import Ajv from "ajv";
+
+import { chat } from "./chat";
+
+const port = Number(process.env.PORT || "4543");
+const server = App();
+
+const ajv = new Ajv({ strict: false });
+
+const wsx = Wsapix.uWS(
+  { server },
+  {
+    validator: (schema, data, error) => {
+      const valid = ajv.validate(schema, data);
+      if (!valid) {
+        error(ajv.errors!.map(({ message }) => message).join(",\n"));
+      }
+      return valid;
+    },
+  }
+);
+
+wsx.route(chat);
+
+wsx.serverMessage(
+  { type: "error" },
+  {
+    $id: "error",
+    description: "Backend error message",
+    payload: Type.Strict(
+      Type.Object({
+        type: Type.String({ const: "error" }),
+        message: Type.String(),
+        code: Type.Optional(Type.Number()),
+      })
+    ),
+  }
+);
+
+wsx.on("error", (ctx, error) => {
+  ctx.send({ type: "error", message: error });
+  console.log(error);
+});
+
+server.get("/", (res) => {
+  res
+    .writeHeader("Content-Type", "text/html")
+    .end(wsx.htmlDocTemplate("/wsapix"));
+});
+
+server.get("/wsapix", (res) => {
+  res.writeHeader("Content-Type", "application/json").end(
+    wsx.asyncapi({
+      info: {
+        version: "1.0.0",
+        title: "Chat websocket API",
+      },
+    })
+  );
+});
+
+server.listen(port, () => {
+  console.log(`Server listen port ${port}`);
+});
